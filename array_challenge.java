8a976eaf59173fae662ad1c3ad5889ad44f6bbc7
TheArchitekt <34973139+TheArchitekt@users.noreply.github.com>
2020-01-25 16:10:23 -0500

    Add files via upload

---
+++ array_challenge.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class array_challenge {
+	public static void main(String[] args) {
+	int[] myIntegers = getIntegers(5);
+	int[] sorted = sortIntegers(myIntegers);
+	printArray(sorted);
+	
+	}
+	
+	public static int[] getIntegers(int number) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int[] values = new int [number];
+		System.out.print("Enter " + number + " values:\r ");
+		for (int i = 0; i < values.length; i++) {
+			values[i] = scanner.nextInt();
+		}
+		
+		return values;
+	}
+	
+	public static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Element " + i + ", contents " + array[i] );
+		}
+	}
+	
+	public static int[] sortIntegers(int[] array) {
+		int[] sortedArray = new int[array.length];
+		
+		for (int i = 0; i < array.length; i++) {
+			sortedArray[i] = array[i];
+		}
+		boolean flag = true;
+		int temp;
+		
+		while (flag) {
+			flag = false;
+			for (int i = 0; i < sortedArray.length -1; i++) {
+				if(sortedArray[i] < sortedArray[i + 1]) {
+					temp = sortedArray[i];
+					sortedArray[i] = sortedArray[i + 1];
+					sortedArray[i + 1] = temp;
+					flag = true;
+				}
+			}
+		}
+		return sortedArray;
+	}
+}
